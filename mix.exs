d546e1dea3b4725259d38d28ba38d9ab0de6cbda
kacper.muryn <kacper@catalyx.io>
2024-03-02 17:26:20 +0100

    Install UUID Library

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule PollApp.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:uuid, "~> 1.1"}
     ]
   end
 
